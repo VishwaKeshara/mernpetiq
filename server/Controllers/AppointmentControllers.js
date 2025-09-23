3ed74f96d39d399d669534b50b0f7b7b95785b98
Vishwa Keshara <vishk1027@gmail.com>
2025-09-24 04:23:23 +0530

    Update AppointmentControllers.js

--- server/Controllers/AppointmentControllers.js
+++ server/Controllers/AppointmentControllers.js
@@ -14,23 +14,52 @@ const handleAppointmentContoller = async (req, res) => {
         ) {
             return res
             .status(400)
-            .json({message: "All fields are required", Success: false});
+            .json({message: "All fields are required", success: false});
         }
         const appointmentAdd = await Appointment.create(body);
 
         if(appointmentAdd){
-            return res
+          return res
             .status(201)
-            .json({message: "Data Created Succsefully", Success: true, Id: appointmentAdd?._id});
+            .json({message: "Data Created Sucssefully", success: true, Id:appointmentAdd?._id, });
         }
 
         console.log("appointmentAdd", appointmentAdd);
-
     }catch(error){
      return res
         .status(500)
         .json({message: error.message, Success: false});
     }
 };
+const handleAppointmentListContoller = async (req, res) => { 
+    try{
+        const appointmentList =await Appointment.find({});
+        return res
+        .status(200)
+        .json({message: "All Appointmnets fetched successfully", Success: true, TotalCount: appointmentList.length, appointmentList:appointmentList });
+        
+    }catch(error){
+        return res
+        .status(400)
+        .json({message: error.message, Success: false});
+    }    
+};
+const handleAppointmentDeleteContoller = async (req, res) => { 
+   const body = req.body;
+    try{
+        const deleted =await Appointment.deleteOne({_id: body.Id});
+        console.log("deleted", deleted);
+        if(deleted.acknowledged){
+        return res.json({
+          message: "Appointment Deleted successfully", 
+          success: true});
+        }
+        }catch(error){
+          return res
+        .status(400)
+        .json({message: error.message, success: false});
+    }   
+} ;
 
-module.exports = {handleAppointmentContoller};
+
+module.exports = {handleAppointmentContoller, handleAppointmentListContoller, handleAppointmentDeleteContoller};
