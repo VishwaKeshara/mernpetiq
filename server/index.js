87370d3cc96945ece549bea625b794b1b8ae9c25
Vishwa Keshara <vishk1027@gmail.com>
2025-09-24 04:23:16 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,18 +1,22 @@
-const  express = require("express");
 const databaseconnection = require("./database");
+const  express = require("express");
+const cors = require("cors");
 const appointmentRouter = require("./routes/AppointmentRoutes");
+
 
 databaseconnection();
 
 const app = express();
 
 app.use(express.json())
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("API is running...");
 } );
 
 app.use('/appointment', appointmentRouter)
+
 
 app.listen(8000, () => {
     console.log("Server is running on port 8000");
