c4154d30f6c9ce3ec5b832d72360e2836abffe2e
Vishwa Keshara <vishk1027@gmail.com>
2025-09-24 04:23:19 +0530

    Update AppointmentRoutes.js

--- server/routes/AppointmentRoutes.js
+++ server/routes/AppointmentRoutes.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 //Insert Appointment Controller
-const {handleAppointmentContoller} = require("../Controllers/AppointmentControllers");
+const {handleAppointmentContoller, handleAppointmentListContoller, handleAppointmentDeleteContoller} = require("../Controllers/AppointmentControllers");
 
 //Routes
 router.post("/addAppointment", handleAppointmentContoller);
-
+router.get("/appointmentList", handleAppointmentListContoller);
+router.post("/deleteAppointment", handleAppointmentDeleteContoller);
 
 module.exports = router;
