6fcf9e3297d3181ec64145d39b98ee7615fa16c1
Vishwa Keshara <vishk1027@gmail.com>
2025-09-23 16:06:16 +0530

    Update AppointmentControllers.js

--- server/Controllers/AppointmentControllers.js
+++ server/Controllers/AppointmentControllers.js
@@ -1,111 +1,36 @@
-// const express = require("express");
-// const router = express.Router();
 const Appointment = require("../Model/AppointmentModel");
 
-const getAllAppointments = async (req, res, next) => {
-    let appointments;
-    //Get All Appointments
-    try {
-        appointments = await Appointment.find();
-    } catch (err) {
-        console.log(err);
+const handleAppointmentContoller = async (req, res) => { 
+    try{
+        const body = req.body;
+        if(!body.ownerName || 
+            !body.petName || 
+            !body.petType || 
+            !body.service || 
+            !body.price || 
+            !body.vet || 
+            !body.date || 
+            !body.time
+        ) {
+            return res
+            .status(400)
+            .json({message: "All fields are required", Success: false});
+        }
+        const appointmentAdd = await Appointment.create(body);
+
+        if(appointmentAdd){
+            return res
+            .status(201)
+            .json({message: "Data Created Succsefully", Success: true, Id: appointmentAdd?._id});
+        }
+
+        console.log("appointmentAdd", appointmentAdd);
+
+    }catch(error){
+     return res
+        .status(500)
+        .json({message: error.message, Success: false});
     }
-    //If no appointments found
-    if (!appointments) {
-        return res.status(404).json({ message: "No Appointments Found" });
-    }
-    //Display all appointments
-    return res.status(200).json({ appointments });
 };
 
-//Data insert
-    const addAppointments = async (req, res, next) => {
-    const { ownerName, petName, petType, service, price, vet, date, time } = req.body;
-
-    let appointment;    
-    try {
-        appointment = new Appointment({
-            ownerName,
-            petName,
-            petType,
-            service,
-            price,
-            vet,
-            date,
-            time
-        });
-        await appointment.save();
-    } catch (err) {
-        console.log(err);
-    }       
-    if (!appointment) {
-        return res.status(404).json({ message: "Unable to add" });
-    }
-    return res.status(200).json({ appointment });
-
-};
-
-//Get by ID
-const getById = async (req, res, next) => {
-
-    const id = req.params.id;
-
-    let appointment;  
-
-    try {
-        appointment = await Appointment.findById(id);
-    } catch (err) {
-        console.log(err);
-    }
-    if (!appointment) {
-        return res.status(404).json({ message: "No Appointment Found" });
-    }
-    return res.status(200).json({ appointment });
-};
-//Update Appointment details
-const updateAppointment = async (req, res, next) => {
-    const id = req.params.id;
-    const { ownerName, petName, petType, service, price, vet, date, time } = req.body;
-    let appointment;
-    try {
-        appointment = await Appointment.findByIdAndUpdate(id, {
-            ownerName: ownerName,
-            petName: petName,
-            petType: petType,
-            service: service,
-            price: price,
-            vet: vet,
-            date: date,
-            time: time
-        });
-        appointment = await appointment.save();
-    } catch (err) {
-        console.log(err);
-    }
-    if (!appointment) {
-        return res.status(404).json({ message: "Unable to update by this ID" });
-    }
-    return res.status(200).json({ appointment });
-};
-//Delete Appointment details
-const deleteAppointment = async (req, res, next) => {
-    const id = req.params.id;
-    let appointment;    
-    try {
-        appointment = await Appointment.findByIdAndDelete(id);
-    }
-    catch (err) {
-        console.log(err);
-    }   
-    if (!appointment) {
-        return res.status(404).json({ message: "Unable to delete by this ID" });
-    }
-    return res.status(200).json({ message: "Appointment Successfully Deleted" });
-};
-
-//Export all functions  
-exports.getAllAppointments = getAllAppointments;
-exports.addAppointments = addAppointments;
-exports.getById = getById;
-exports.updateAppointment = updateAppointment;
-exports.deleteAppointment = deleteAppointment;
+module.exports = {handleAppointmentContoller};
