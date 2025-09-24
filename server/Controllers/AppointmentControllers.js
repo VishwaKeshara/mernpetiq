ae75b2bce3f67e20513adef39ff52d7d8254e38b
Vishwa Keshara <vishk1027@gmail.com>
2025-09-24 15:31:57 +0530

    Update AppointmentControllers.js

--- server/Controllers/AppointmentControllers.js
+++ server/Controllers/AppointmentControllers.js
@@ -1,65 +1,103 @@
 const Appointment = require("../Model/AppointmentModel");
 
-const handleAppointmentContoller = async (req, res) => { 
-    try{
+// Create Appointment
+const handleAppointmentController = async (req, res) => { 
+    try {
         const body = req.body;
-        if(!body.ownerName || 
-            !body.petName || 
-            !body.petType || 
-            !body.service || 
-            !body.price || 
-            !body.vet || 
-            !body.date || 
-            !body.time
-        ) {
+        const { ownerName, petName, petType, service, price, vet, date, time } = body;
+
+        if (!ownerName || !petName || !petType || !service || !price || !vet || !date || !time) {
             return res
-            .status(400)
-            .json({message: "All fields are required", success: false});
+                .status(400)
+                .json({ message: "All fields are required", success: false });
         }
+
         const appointmentAdd = await Appointment.create(body);
 
-        if(appointmentAdd){
-          return res
-            .status(201)
-            .json({message: "Data Created Sucssefully", success: true, Id:appointmentAdd?._id, });
+        if (appointmentAdd) {
+            return res
+                .status(201)
+                .json({ 
+                    message: "Data created successfully", 
+                    success: true, 
+                    id: appointmentAdd._id 
+                });
         }
-
-        console.log("appointmentAdd", appointmentAdd);
-    }catch(error){
-     return res
-        .status(500)
-        .json({message: error.message, Success: false});
+    } catch (error) {
+        return res
+            .status(500)
+            .json({ message: error.message, success: false });
     }
 };
-const handleAppointmentListContoller = async (req, res) => { 
-    try{
-        const appointmentList =await Appointment.find({});
+
+// Get All Appointments
+const handleAppointmentListController = async (req, res) => { 
+    try {
+        const appointmentList = await Appointment.find({});
         return res
-        .status(200)
-        .json({message: "All Appointmnets fetched successfully", Success: true, TotalCount: appointmentList.length, appointmentList:appointmentList });
-        
-    }catch(error){
+            .status(200)
+            .json({ 
+                message: "All appointments fetched successfully", 
+                success: true, 
+                totalCount: appointmentList.length, 
+                appointmentList 
+            });
+    } catch (error) {
         return res
-        .status(400)
-        .json({message: error.message, Success: false});
+            .status(500)
+            .json({ message: error.message, success: false });
     }    
 };
-const handleAppointmentDeleteContoller = async (req, res) => { 
-   const body = req.body;
-    try{
-        const deleted =await Appointment.deleteOne({_id: body.Id});
-        console.log("deleted", deleted);
-        if(deleted.acknowledged){
-        return res.json({
-          message: "Appointment Deleted successfully", 
-          success: true});
+
+// Delete Appointment
+const handleAppointmentDeleteController = async (req, res) => { 
+    try {
+        const { id } = req.body;
+        if (!id) {
+            return res.status(400).json({ message: "Appointment ID is required", success: false });
         }
-        }catch(error){
-          return res
-        .status(400)
-        .json({message: error.message, success: false});
+
+        const deleted = await Appointment.deleteOne({ _id: id });
+
+        if (deleted.deletedCount > 0) {
+            return res.json({ message: "Appointment deleted successfully", success: true });
+        } else {
+            return res.status(404).json({ message: "Appointment not found", success: false });
+        }
+    } catch (error) {
+        return res.status(500).json({ message: error.message, success: false });
     }   
-} ;
+};
 
+// Update Appointment
+const handleAppointmentUpdateController = async (req, res) => { 
+    try {
+        const body = req.body;
+        const { _id } = body;
 
-module.exports = {handleAppointmentContoller, handleAppointmentListContoller, handleAppointmentDeleteContoller};
+        if (!_id) {
+            return res.status(400).json({ message: "Appointment ID is required", success: false });
+        }
+
+        const updating = await Appointment.updateOne({ _id }, { $set: body });
+
+        if (updating.matchedCount === 0) {
+            return res.status(404).json({ message: "Appointment not found", success: false });
+        }
+
+        if (updating.modifiedCount > 0) {
+            return res.json({ message: "Appointment updated successfully", success: true });
+        } else {
+            return res.json({ message: "No changes made", success: true });
+        }
+    } catch (error) {
+        return res.status(500).json({ message: error.message, success: false });
+    }   
+};
+
+module.exports = {
+    handleAppointmentController,
+    handleAppointmentListController,
+    handleAppointmentDeleteController,
+    handleAppointmentUpdateController
+};
