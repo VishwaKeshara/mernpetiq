84ba5c9b051764e30a58aed3ff3109d9b128aca6
Nethmika Abeysinghe <abeysinghenethmika@gmail.com>
2025-10-06 23:11:18 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,10 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { Elements } from '@stripe/react-stripe-js'
+import { loadStripe } from '@stripe/stripe-js'
+
+const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY)
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Elements stripe={stripePromise}>
       <App />
+    </Elements>
   </StrictMode>,
 )
